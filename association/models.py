35d57a906c2f63681b03ef97f81c190f8026a4eb
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2016-04-22 19:26:15 +0200

    added Meta class to assocation Member model

--- association/models.py
+++ association/models.py
@@ -6,5 +6,10 @@ class Member(models.Model):
     surname = models.CharField(_('surname'), max_length=200)
     name = models.CharField(_('name'), max_length=200)
 
+    class Meta:
+        verbose_name = _('member')
+        verbose_name_plural = _('members')
+        ordering = ('surname', 'name')
+
     def __str__(self):
         return '{name} {surname}'.format(surname=self.surname, name=self.name)
