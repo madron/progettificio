8593072234e2dc8feb31212c61e2055bb2947d04
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2016-04-24 00:19:33 +0200

    moved chmod in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
+chown -R uwsgi:uwsgi /media
+
 if [ "$1" = 'uwsgi' ]; then
     chown -R uwsgi:uwsgi /run/uwsgi
-    chown -R uwsgi:uwsgi /media
     gosu uwsgi /src/manage.py migrate --noinput
     exec gosu uwsgi uwsgi "${@:2}"
 elif [ "$1" = 'nginx' ]; then
